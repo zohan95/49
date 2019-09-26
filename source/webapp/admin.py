5a04398920d9c4e8f02d08419db9a1b6a67bc396
Ваше Имя <you@example.com>
2019-09-26 10:49:10 +0600

    stage 1 done

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(TaskStatus)
+admin.site.register(TaskType)
+admin.site.register(Task)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskStatus(models.Model):
+    status = models.CharField(max_length=100, verbose_name='Статус')
+
+    def __str__(self):
+        return self.status
+
+
+class TaskType(models.Model):
+    type = models.CharField(max_length=100, verbose_name='Тип')
+
+    def __str__(self):
+        return self.type
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=100, verbose_name='Краткое описание')
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Полное описание')
+    taks_status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT)
+    task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT)
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.summary[:20]
+
+
+
