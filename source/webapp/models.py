445520b056efc82733af014a2c9df6ccf516d380
Ваше Имя <you@example.com>
2019-10-11 10:40:28 +0600

    Добавлена модель Проектов

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -21,9 +21,16 @@ class Task(models.Model):
     task_status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT, verbose_name='Статус')
     task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT, verbose_name='Тип')
     date_create = models.DateTimeField(auto_now_add=True)
+    project = models.ForeignKey('webapp.Project', verbose_name='Проект', related_name='projects', blank=True, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.summary[:20]
 
+
+class Project(models.Model):
+    summary = models.CharField(max_length=100, verbose_name='Название проекта')
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Описание')
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
 
 
