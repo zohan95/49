e5a59ebfe51664e6bdf3b875ef0edc95bfe11aff
zalkar <zalkarvk@gmail.com>
2019-10-25 10:34:23 +0600

    Выполнена доп часть

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,19 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import resolve
 
+redirect_url = ''
 
 def login_view(request):
     context = {}
+    global redirect_url
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('webapp:main_url')
+            return redirect(redirect_url)
         else:
             context['has_error'] = True
-    return render(request, 'login.html', context=context)
+
+    if request.method == 'GET':
+        redirect_url = request.GET.get('next')
+        return render(request, 'login.html', context=context)
 
 
 def logout_view(request):
