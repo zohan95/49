9564b3a8e78ecea0b65e0f8ff5b5b489b8b9b852
Ваше Имя <you@example.com>
2019-11-02 20:27:52 +0600

    Сделаны миграции, обновлен gitignore

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-from uuid import uuid4
+from django.contrib.auth.models import User
+
+# class Token(models.Model):
+#     token = models.UUIDField(verbose_name='Token', default=uuid4)
+#     user = models.ForeignKey('auth.User', related_name='registration_tokens',
+#                              verbose_name='User', on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return str(self.token)
 
 
-class Token(models.Model):
-    token = models.UUIDField(verbose_name='Token', default=uuid4)
-    user = models.ForeignKey('auth.User', related_name='registration_tokens',
-                             verbose_name='User', on_delete=models.CASCADE)
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
 
     def __str__(self):
-        return str(self.token)
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
