2e830ad3a26068bbb8fe09415dfd7efd681f9c54
Ваше Имя <you@example.com>
2019-11-02 20:35:46 +0600

    обновлена админка и страница просмотра профиля

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.urls import path, include
+from django.conf import settings
 
 
  
@@ -13,4 +15,4 @@ urlpatterns = [
 
    path('', include('webapp.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
